433de0c472541ed9ab0f48f2613896e75c15dbb4
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2023-09-20 23:09:50 +0200

    Update bulk-webp-png-convert.ps1
    Add Use Guide

--- Scripts for Stable Diffusion/bulk-webp-png-convert.ps1
+++ Scripts for Stable Diffusion/bulk-webp-png-convert.ps1
@@ -1,4 +1,18 @@
-# This script searches the specified folder for WebP files and converts them to PNG, which makes training LoRAs easier.
+# Usage Guide:
+# This script is designed to convert WebP files to PNG format within a specified directory. This can be particularly useful when preparing datasets for training LoRAs, as it simplifies the process by automating the conversion of image files to a more commonly used format.
+# Instructions:
+# 1. Ensure that Python and the PIL library are installed in your environment.
+# 2. Save this script as a .py file (e.g., convert_webp_to_png.py).
+# 3. Run the script in a terminal or an IDE.
+# 4. When prompted, enter the absolute path to the folder containing the WebP files you wish to convert.
+# 5. The script will then iterate through the folder, converting each WebP file to PNG format and deleting the original WebP files.
+# 6. A message will be displayed upon successful completion of the conversion process.
+# Note: 
+# - The script only searches for files with a ".webp" extension.
+# - Original .webp files will be deleted after conversion to avoid duplication.
+# Example:
+# To use this script, open a terminal and navigate to the directory where the script is located. Then, run the script using the following command:
+#     python convert_webp_to_png.py
 
 from PIL import Image
 import os
